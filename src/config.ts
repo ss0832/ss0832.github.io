9d12dc552cbc4475a862f8c89779d698353a1158
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-07-08 13:53:34 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,8 @@ export const siteConfig: SiteConfig = {
   subtitle: 'Personal Web Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 260,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
