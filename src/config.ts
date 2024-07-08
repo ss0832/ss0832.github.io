c262526c4ec11e2e3e2c41e0719acdf5f1f71cad
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-07-08 13:23:30 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/ss0832',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
@@ -43,21 +43,14 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'ss0832',
+  bio: "This is ss0832's website.",
   links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
+          // Visit https://icones.js.org/ for icon codes
+          // You will need to install the corresponding icon set if it's not already included
+          // `pnpm add @iconify-json/<icon-set-name>`
+ 
+
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
